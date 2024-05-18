2252c5114dc5043db725674864633b92e2bd895e
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-05-18 20:19:48 +0300

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,10 @@ class BaseModel():
         if kwargs:
             self.__dict__ = kwargs.copy()
             del self.__dict__['__class__']
-            self.__dict__['updated_at'] = datetime.fromisoformat(kwargs['updated_at'])
-            self.__dict__['created_at'] = datetime.fromisoformat(kwargs['created_at'])
+            updated_at = datetime.fromisoformat(kwargs['updated_at'])
+            self.__dict__['updated_at'] = updated_at
+            created_at = datetime.fromisoformat(kwargs['created_at'])
+            self.__dict__['created_at'] = created_at
         else:
             self.id = str(uuid.uuid4())
             self.created_at = (datetime.now())
