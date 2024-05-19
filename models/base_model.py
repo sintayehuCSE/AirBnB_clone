1ea81555c5a6c889cb5bd5f658e9486f60b22e4f
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-05-19 17:12:35 +0300

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This is a module that define a base model class of the project."""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -23,10 +24,12 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = (datetime.now())
             self.updated_at = (datetime.now())
+            storage.new(self)
 
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
