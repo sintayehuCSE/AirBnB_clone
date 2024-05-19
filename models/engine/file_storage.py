1f31d1d2130988b2eacfc0f365bffa191e6faa5f
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-05-19 18:01:00 +0300

    File storage engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,24 +17,27 @@ class FileStorage():
     """
     __file_path = 'file.json'
     __objects = {}
-    
+
     def all(self):
         """Returns the dictionary __objects"""
         return FileStorage.__objects
-        
+
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
-    
+
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
-            new_dict = {}
-            for key in FileStorage.__objects.keys():
-                new_dict[key] = FileStorage.__objects[key].to_dict()
-            json.dump(new_dict, f)
-     
+        try:
+            with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
+                new_dict = {}
+                for key in FileStorage.__objects.keys():
+                    new_dict[key] = FileStorage.__objects[key].to_dict()
+                json.dump(new_dict, f)
+        except IsADirectoryError:
+            pass
+
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file
             (__file_path) exists ; otherwise, do nothing. If the file doesn’t
