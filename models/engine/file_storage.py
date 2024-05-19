6e9fde9a609cc3e4dc0a31a0e922e77ba476873b
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-05-19 18:16:33 +0300

    Why

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ A class for file storage Engine."""
 import json
+import os
 from datetime import datetime
 
 
@@ -43,6 +44,8 @@ class FileStorage():
             (__file_path) exists ; otherwise, do nothing. If the file doesn’t
             exist, no exception should be raised)
         """
+        if not os.path.isfile(FileStorage.__file_path):
+            return
         try:
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
                 new_dict = json.load(f)
