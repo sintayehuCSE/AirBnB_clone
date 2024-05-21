fe93a485beaf6c75ff8ff051bcf22600f6822a20
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-05-21 15:50:13 +0300

    Test file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -14,13 +14,29 @@ class Test_FileStorage(unittest.TestCase):
         """Test creation of storage class object."""
         store_obj = FileStorage()
         self.assertIsInstance(store_obj, FileStorage)
+    
+    def test_FileStorage_all(self):
+        """Check if all method is defined for the Storage class."""
+        self.assertIsNotNone(FileStorage.all)
+    
+    def Test_FileStorage_new(self):
+        """Check for presence of new() method in FileStorage class."""
+        self.assertIsNotNone(FileStorage.new)
+
+    def Test_FileStorage_save(self):
+        """Check for presence of save() method in FileStorage class."""
+        self.assertIsNotNone(FileStorage.save)
+
+    def Test_FileStorage_reload(self):
+        """Check for presence of reload() method in FileStorage class."""
+        self.assertIsNotNone(FileStorage.reload)
 
     def test_doc(self):
         """Check if sTorage function are documented or not."""
-        self.assertIsNotNone(FileStorage.all)
-        self.assertIsNotNone(FileStorage.new)
-        self.assertIsNotNone(FileStorage.save)
-        self.assertIsNotNone(FileStorage.reload)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
 
     def test_type__file_path(self):
         """check for file_path type as only string."""
