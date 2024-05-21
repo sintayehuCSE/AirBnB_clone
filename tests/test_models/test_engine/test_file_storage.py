cb5ce8addd142742fb047734efb39c654df0c465
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-05-21 17:28:46 +0300

    Test FileStorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Test for storage
+"""
+from datetime import datetime
+import unittest
+from time import sleep
+import json
+from models.engine.file_storage import FileStorage
+
+
+class Test_FileStorage(unittest.TestCase):
+    """Test the FileStorage Engine class."""
+    def test_obje_creation(self):
+        """Check cretion of Storage object."""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_FileStorage__objects(self):
+        """Test the type of __object container holding live objecct."""
+        self.assertIs(dict, type(FileStorage._FileStorage__objects))
+
+    def test_FileStorage__file_path(self):
+        """Test the type of __file_path of Storage engine."""
+        self.assertIs(str, type(FileStorage._FileStorage__file_path))
+
+    def test_FileStorage_all(self):
+        """check presence of all() method in Engine file."""
+        self.assertIsNotNone(FileStorage.all)
+    
+    def test_FileStorage_new(self):
+        """check presence of new() method in Engine file.""" 
+        self.assertIsNotNone(FileStorage.new)
+
+    def test_FileStorage_save(self):
+        """check presence of save() method in Engine file."""
+        self.assertIsNotNone(FileStorage.save)
+
+    def test_FileStorage_reload(self):
+        """check presence of reload() method in Engine file."""
+        self.assertIsNotNone(FileStorage.reload)
+
+    def test_method_documented(self):
+        """Check if method of thee class are documented."""
+        self.assertIsNotNone(FileStorage.all.__dict__)
+        self.assertIsNotNone(FileStorage.new.__dict__)
+        self.assertIsNotNone(FileStorage.save.__dict__)
+        self.assertIsNotNone(FileStorage.reload.__dict__)        
